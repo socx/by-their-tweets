60dff7ab7b3a7fae80784d2db666151b355bef73
socx <socx9@yahoo.com>
2021-01-06 00:03:27 +0000

    delete dead code

--- src/container/Faq.js
+++ src/container/Faq.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FaqComponent from 'components/Faq';
-import Spinner from 'components/Spinner';
 
 class Faq extends React.Component {
   scrollToTop = () => window.scrollTo(0, 0);
